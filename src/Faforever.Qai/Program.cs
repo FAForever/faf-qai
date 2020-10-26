b0da1cf75d1ee0e1d55e9fd0d7e255934f19b0e1
Christian Sindberg <chrsin@gmail.com>
2020-10-26 12:57:39 +0100

    #2 Started adding a simple IrcImplementation

--- src/Faforever.Qai/Program.cs
+++ src/Faforever.Qai/Program.cs
@@ -1,20 +1,29 @@
 using System;
-
+using Faforever.Qai.Irc;
+using IrcDotNet;
 using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace Faforever.Qai
-{
-	public static class Program
-	{
-		public static int Main(string[] args)
-		{
+namespace Faforever.Qai {
+	public static class Program {
+		public static int Main(string[] args) {
 			CommandLineApplication app = new CommandLineApplication();
 
 			app.HelpOption("-h|--help");
 
-			app.OnExecuteAsync(async cancellationToken =>
-			{
-				Console.WriteLine("Hello World!");
+			app.OnExecuteAsync(async cancellationToken => {
+				ServiceCollection services = new ServiceCollection();
+				services.AddLogging(options => options.AddConsole());
+
+				await using var serviceProvider = services.BuildServiceProvider();
+
+				using QaIrc ircBot = new QaIrc("irc.faforever.com", new IrcUserRegistrationInfo {
+					NickName = "Balleby", RealName = "balleby", Password = "balleby", UserName = "balleby"
+				}, serviceProvider.GetService<ILogger<QaIrc>>());
+				ircBot.Run();
+
+				Console.ReadLine();
 			});
 
 			return app.Execute(args);
