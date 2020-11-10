0d9cf9d59724148054ea0face966358e0f6c5b90
Andrew Bounds <andybounds@gmail.com>
2020-11-10 08:34:10 -0500

    Simplified Using Statments

--- src/Faforever.Qai.Core/Operations/Player/ApiFetchPlayerStatsOperation.cs
+++ src/Faforever.Qai.Core/Operations/Player/ApiFetchPlayerStatsOperation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Net.Http;
@@ -22,38 +22,34 @@ namespace Faforever.Qai.Core.Operations.Player
 
 		public async Task<FetchPlayerStatsResult> FetchPlayer(string username)
 		{
-			using (Stream? stream =
+			using Stream? stream =
 				await _client.GetStreamAsync(
-					$"/data/player?include=globalRating,ladder1v1Rating,names&filter=login=={username}"))
+					$"/data/player?include=globalRating,ladder1v1Rating,names&filter=login=={username}");
+			using JsonDocument json = await JsonDocument.ParseAsync(stream);
+			JsonElement includedElement = json.RootElement.GetProperty("included");
+			FetchPlayerStatsResult result = new FetchPlayerStatsResult
 			{
-				using (JsonDocument json = await JsonDocument.ParseAsync(stream))
+				Name = username
+			};
+			foreach (JsonElement element in includedElement.EnumerateArray())
+			{
+				var typeElement = element.GetProperty("type");
+				var attributes = element.GetProperty("attributes");
+				switch (typeElement.GetString())
 				{
-					JsonElement includedElement = json.RootElement.GetProperty("included");
-					FetchPlayerStatsResult result = new FetchPlayerStatsResult
-					{
-						Name = username
-					};
-					foreach (JsonElement element in includedElement.EnumerateArray())
-					{
-						var typeElement = element.GetProperty("type");
-						var attributes = element.GetProperty("attributes");
-						switch (typeElement.GetString())
-						{
-							case "ladder1v1Rating":
-								result.Rating1v1 = attributes.GetProperty("rating").GetDecimal();
-								result.Ranking1v1 = attributes.GetProperty("ranking").GetInt16();
+					case "ladder1v1Rating":
+						result.Rating1v1 = attributes.GetProperty("rating").GetDecimal();
+						result.Ranking1v1 = attributes.GetProperty("ranking").GetInt16();
 
-								break;
-							case "globalRating":
-								result.RatingGlobal = attributes.GetProperty("rating").GetDecimal();
-								result.RankingGlobal = attributes.GetProperty("ranking").GetInt16();
-								break;
-						}
-					}
-
-					return result;
+						break;
+					case "globalRating":
+						result.RatingGlobal = attributes.GetProperty("rating").GetDecimal();
+						result.RankingGlobal = attributes.GetProperty("ranking").GetInt16();
+						break;
 				}
 			}
+
+			return result;
 		}
 	}
 }
