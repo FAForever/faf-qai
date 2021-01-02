27f91d277c4699a30e1a1f51f9e12e8b6490f037
Andrew Bounds <andybounds@gmail.com>
2021-01-02 17:07:45 -0500

    Simplified using statements

--- src/Faforever.Qai.Core/Operations/Player/ApiFindPlayerOperation.cs
+++ src/Faforever.Qai.Core/Operations/Player/ApiFindPlayerOperation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Net.Http;
 using System.Text.Json;
@@ -19,28 +19,24 @@ namespace Faforever.Qai.Core.Operations.Player
 		}
 		public async Task<FindPlayerResult> FindPlayer(string searchTerm)
 		{
-			using (Stream? stream =
+			using Stream? stream =
 				await _client.GetStreamAsync(
-					$"/data/player?include=names&filter=login==*{searchTerm}*"))
+					$"/data/player?include=names&filter=login==*{searchTerm}*");
+			using JsonDocument json = await JsonDocument.ParseAsync(stream);
+			JsonElement dataElement = json.RootElement.GetProperty("data");
+			FindPlayerResult result = new FindPlayerResult();
+			foreach (JsonElement element in dataElement.EnumerateArray())
 			{
-				using (JsonDocument json = await JsonDocument.ParseAsync(stream))
-				{
-					JsonElement dataElement = json.RootElement.GetProperty("data");
-					FindPlayerResult result = new FindPlayerResult();
-					foreach (JsonElement element in dataElement.EnumerateArray())
-					{
-						var typeElement = element.GetProperty("type");
-						if (typeElement.GetString() != "player")
-							continue;
+				var typeElement = element.GetProperty("type");
+				if (typeElement.GetString() != "player")
+					continue;
 
-						var attributes = element.GetProperty("attributes");
+				var attributes = element.GetProperty("attributes");
 
-						result.Usernames.Add(attributes.GetProperty("login").GetString());
-					}
-
-					return result;
-				}
+				result.Usernames.Add(attributes.GetProperty("login").GetString());
 			}
+
+			return result;
 		}
 	}
 }
