6cd126267b8542a295bea93683b698048811f4a2
Andrew Bounds <andybounds@gmail.com>
2021-04-06 15:28:18 -0400

    Added command to set the role given when a link is completed.

---
+++ src/Faforever.Qai.Core/Commands/Discord/Link/SetRoleForCompletedLinkCommand.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DSharpPlus.Entities;
+
+using Faforever.Qai.Core.Commands.Authorization;
+using Faforever.Qai.Core.Database;
+using Faforever.Qai.Core.Structures.Configurations;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Qmmands;
+
+namespace Faforever.Qai.Core.Commands.Discord.Link
+{
+	public class SetRoleForCompletedLinkCommand : DiscordCommandModule
+	{
+		private readonly IServiceProvider _services;
+
+		public SetRoleForCompletedLinkCommand(IServiceProvider services)
+		{
+			_services = services;
+		}
+
+		[Command("linkrole", "roleforlinks")]
+		[Description("The role to give a user after they have linked their account. Leave the role blank to disable this feature.")]
+		[RequirePermissions(DSharpPlus.Permissions.ManageRoles)]
+		public async Task SetRoleForCompletedLinkCommandAsync(DiscordRole? role = null)
+		{
+			var db = _services.GetRequiredService<QAIDatabaseModel>();
+			var guild = await db.FindAsync<DiscordGuildConfiguration>(Context.Guild.Id);
+
+			if(guild is null)
+			{
+				guild = new(Context.Guild.Id, "!");
+				await db.AddAsync(guild);
+				await db.SaveChangesAsync();
+			}
+
+			guild.RoleWhenLinked = role?.Id;
+
+			await db.SaveChangesAsync();
+
+			await Context.ReplyAsync($"The role {role?.Mention ?? "`no role`"} will now be given to any member who completes the linking process.");
+		}
+	}
+}
