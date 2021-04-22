a08f2a960b682fd38454c51edd46ee7c3779f499
Andrew Bounds <andybounds@gmail.com>
2021-04-22 14:33:27 -0400

    Added a view link and delete link commands for Discord-FAF links

---
+++ src/Faforever.Qai.Core/Commands/Discord/Link/GetMemberLinkCommand.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DSharpPlus.Entities;
+
+using Faforever.Qai.Core.Commands.Arguments;
+using Faforever.Qai.Core.Commands.Authorization;
+using Faforever.Qai.Core.Database;
+using Faforever.Qai.Core.Structures.Link;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Qmmands;
+
+namespace Faforever.Qai.Core.Commands.Discord.Link
+{
+	public class GetMemberLinkCommand : DiscordCommandModule
+	{
+		private readonly IServiceProvider _services;
+
+		public GetMemberLinkCommand(IServiceProvider services)
+		{
+			_services = services;
+		}
+
+		[Command("links", "getlinks")]
+		[RequireFafStaff]
+		public async Task GetMemberLinkCommandAsync(DiscordUser user)
+		{
+			var db = _services.GetRequiredService<QAIDatabaseModel>();
+			var link = await db.FindAsync<AccountLink>(user.Id);
+
+			if(link is null)
+			{
+				await Context.Client.SendMessageAsync(Context.Channel, new DiscordEmbedBuilder()
+					.WithColor(DiscordColor.DarkRed)
+					.WithDescription($"No link found for this member."));
+			}
+			else
+			{
+				await Context.Client.SendMessageAsync(Context.Channel, GetLinkDataEmbed(link)
+						.WithColor(Context.DostyaRed));
+			}
+		}
+
+		public static DiscordEmbedBuilder GetLinkDataEmbed(AccountLink link)
+		{
+			return new DiscordEmbedBuilder()
+					.WithTitle($"Link for {link.DiscordUsername}")
+					.AddField("Discord", $"```\n" +
+					$"Username : {link.DiscordUsername}\n" +
+					$"ID       : {link.DisocrdId}" +
+					$"\n```", true)
+					.AddField("FAF", $"```\n" +
+					$"Username : {link.FafUsername}\n" +
+					$"ID       : {link.FafId}" +
+					$"\n```", true);
+		}
+	}
+}
---
+++ src/Faforever.Qai.Core/Commands/Discord/Link/UnlinkMemberCommand.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DSharpPlus.Entities;
+
+using Faforever.Qai.Core.Commands.Arguments;
+using Faforever.Qai.Core.Commands.Authorization;
+using Faforever.Qai.Core.Database;
+using Faforever.Qai.Core.Structures.Link;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Qmmands;
+
+namespace Faforever.Qai.Core.Commands.Discord.Link
+{
+	public class UnlinkMemberCommand : DiscordCommandModule
+	{
+		private readonly IServiceProvider _services;
+
+		public UnlinkMemberCommand(IServiceProvider services)
+		{
+			_services = services;
+		}
+
+		[Command("unlink")]
+		[Description("Force unlink a member and their FAF account.")]
+		[RequireFafStaff]
+		public async Task UnlinkMemberLinkCommandAsync(DiscordUser user)
+		{
+			var db = _services.GetRequiredService<QAIDatabaseModel>();
+			var link = await db.FindAsync<AccountLink>(user.Id);
+
+			if (link is null)
+			{
+				await Context.Client.SendMessageAsync(Context.Channel, new DiscordEmbedBuilder()
+					.WithColor(DiscordColor.DarkRed)
+					.WithDescription($"No link found for this member."));
+			}
+			else
+			{
+				db.Remove(link);
+				await db.SaveChangesAsync();
+
+				await Context.Client.SendMessageAsync(Context.Channel,
+					GetMemberLinkCommand.GetLinkDataEmbed(link)
+						.WithColor(Context.DostyaRed)
+						.WithTitle($"Old link for {user.Username}")
+						.WithAuthor("Unlinked account - displaying link details"));
+			}
+		}
+	}
+}
