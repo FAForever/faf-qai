4e546e02bfc96d8b81dc511a2faa2d2c4cd6bb2e
Crotalus <Crotalus@users.noreply.github.com>
2021-08-03 13:30:58 +0200

    Fixed null exception

--- src/Faforever.Qai.Core/Extensions/ListExtensions.cs
+++ src/Faforever.Qai.Core/Extensions/ListExtensions.cs
@@ -8,7 +8,7 @@ namespace Faforever.Qai.Core.Extensions
 {
 	public static class ListExtensions
 	{
-		private static readonly Random _rand;
+		private static readonly Random _rand = new();
 
 		public static T Random<T>(this List<T> list)
 			=> list[_rand.Next(0, list.Count)];
