95382ffbf67dfd6e1dceda6d92f686e755e468a7
Crotalus <Crotalus@users.noreply.github.com>
2022-11-05 19:16:06 +0100

    Handle user response without includes

--- src/Faforever.Qai.Core/Operations/Player/ApiFetchPlayerStatsOperation.cs
+++ src/Faforever.Qai.Core/Operations/Player/ApiFetchPlayerStatsOperation.cs
@@ -38,51 +38,54 @@ namespace Faforever.Qai.Core.Operations.Player
                 Id = json.RootElement.GetProperty("data")[0].GetProperty("id").GetString() ?? ""
             };
 
-            JsonElement includedElement = json.RootElement.GetProperty("included");
-
-            foreach (JsonElement element in includedElement.EnumerateArray())
+            if(json.RootElement.TryGetProperty("included", out var includedElement))
             {
-                var typeElement = element.GetProperty("type");
-                var attributes = element.GetProperty("attributes");
-                switch (typeElement.GetString())
+                foreach (JsonElement element in includedElement.EnumerateArray())
                 {
-                    case "ladder1v1Rating":
-                        var ladder = new GameStatistics
-                        {
-                            Ranking = attributes.GetProperty("ranking").GetInt16(),
-                            Rating = attributes.GetProperty("rating").GetDecimal(),
-                            GamesPlayed = attributes.GetProperty("numberOfGames").GetInt16()
-                        };
-                        result.LadderStats = ladder;
-                        break;
-                    case "globalRating":
-                        var global = new GameStatistics
-                        {
-                            Ranking = attributes.GetProperty("ranking").GetInt16(),
-                            Rating = attributes.GetProperty("rating").GetDecimal(),
-                            GamesPlayed = attributes.GetProperty("numberOfGames").GetInt16()
-                        };
-                        result.GlobalStats = global;
-                        break;
-                    case "clan":
-                        var clan = new FAFClan
-                        {
-                            Name = attributes.GetProperty("name").GetString(),
-                            Size = element.GetProperty("relationships")
-                                .GetProperty("memberships")
-                                .GetProperty("data")
-                                .GetArrayLength(),
-                            URL = attributes.GetProperty("websiteUrl").GetString()
-                        };
-                        result.Clan = clan;
-                        break;
-                    case "nameRecord":
-                        var name = attributes.GetProperty("name").GetString();
-                        if(name is not null)
-                            result.OldNames.Add(name);
-                        break;
+                    var typeElement = element.GetProperty("type");
+                    var attributes = element.GetProperty("attributes");
+                    switch (typeElement.GetString())
+                    {
+                        case "ladder1v1Rating":
+                            var ladder = new GameStatistics
+                            {
+                                Ranking = attributes.GetProperty("ranking").GetInt16(),
+                                Rating = attributes.GetProperty("rating").GetDecimal(),
+                                GamesPlayed = attributes.GetProperty("numberOfGames").GetInt16()
+                            };
+                            result.LadderStats = ladder;
+                            break;
+                        case "globalRating":
+                            var global = new GameStatistics
+                            {
+                                Ranking = attributes.GetProperty("ranking").GetInt16(),
+                                Rating = attributes.GetProperty("rating").GetDecimal(),
+                                GamesPlayed = attributes.GetProperty("numberOfGames").GetInt16()
+                            };
+                            result.GlobalStats = global;
+                            break;
+                        case "clan":
+                            var clan = new FAFClan
+                            {
+                                Name = attributes.GetProperty("name").GetString(),
+                                Size = element.GetProperty("relationships")
+                                    .GetProperty("memberships")
+                                    .GetProperty("data")
+                                    .GetArrayLength(),
+                                URL = attributes.GetProperty("websiteUrl").GetString()
+                            };
+                            result.Clan = clan;
+                            break;
+                        case "nameRecord":
+                            var name = attributes.GetProperty("name").GetString();
+                            if (name is not null)
+                                result.OldNames.Add(name);
+                            break;
+                    }
                 }
             }
+
+            
 
             return result;
         }
